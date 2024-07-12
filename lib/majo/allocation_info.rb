ea5c5491f84fa342ed8449d34cd968f72f9623e2
Masataka Pocke Kuwabara <kuwabara@pocke.me>
2024-07-12 17:08:29 +0900

    Grouping CSV format
    The raw data can be too large. It needs grouping

--- lib/majo/allocation_info.rb
+++ lib/majo/allocation_info.rb
@@ -3,5 +3,21 @@ module Majo
     def inspect
       "#<Majo::AllocationInfo #{object_class_path} #{class_path}##{method_id} #{path}:#{line} gen#{alloc_generation}..#{free_generation}>"
     end
+
+    def hash
+      path.hash ^ class_path.hash ^ method_id.hash ^ line.hash ^ object_class_path.hash ^ alloc_generation.hash ^ free_generation.hash ^ memsize.hash
+    end
+
+    def eql?(other)
+      other.is_a?(AllocationInfo) &&
+        path == other.path &&
+        class_path == other.class_path &&
+        method_id == other.method_id &&
+        line == other.line &&
+        object_class_path == other.object_class_path &&
+        alloc_generation == other.alloc_generation &&
+        free_generation == other.free_generation &&
+        memsize == other.memsize
+    end
   end
 end
--- lib/majo/formatter/csv.rb
+++ lib/majo/formatter/csv.rb
@@ -9,11 +9,15 @@ module Majo
 
       def call
         ::CSV.generate do |csv|
-          csv << ['Object class path', 'Class path', 'Method ID', 'Path', 'Line', 'Alloc generation', 'Free generation', 'Memsize']
-          @result.allocations.each do |row|
-            csv << [row.object_class_path, row.class_path, row.method_id, row.path, row.line, row.alloc_generation, row.free_generation, row.memsize]
+          csv << ['Object class path', 'Class path', 'Method ID', 'Path', 'Line', 'Alloc generation', 'Free generation', 'Memsize', "Count"]
+          groups.each do |row, count|
+            csv << [row.object_class_path, row.class_path, row.method_id, row.path, row.line, row.alloc_generation, row.free_generation, row.memsize, count]
           end
         end
+      end
+
+      def groups
+        @result.allocations.tally
       end
     end
   end
